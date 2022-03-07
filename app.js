229a0bd8bff8642eb2703ab75ecb16b85efd08db
Sohan Das <f20180848@hyderabad.bits-pilani.ac.in>
2022-03-07 12:16:44 +0530

    updated

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use("/tenant", tenantRouter);
 
 
 
+let port = process.env.PORT;
+if (port === null || port === "") {
+    port = 3000;
+}
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("Server started on port 3000");
 })
